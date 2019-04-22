70ac1bcbce4a3091b8e2f6bf617c4f2ec657680e
iwulai <iwulai@qq.com>
2019-04-22 23:55:42 +0800

    init

--- src/AuthModel.php
+++ src/AuthModel.php
@@ -6,7 +6,7 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as Authenticate;
 
-class AuthModel extends Model implements Authenticate
+abstract class AuthModel extends Model implements Authenticate
 {
     use Authenticatable;
 }
