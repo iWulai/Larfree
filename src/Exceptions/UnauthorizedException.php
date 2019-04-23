64a21c1a505c621122557b9d7b1c65e42e7f9522
iwulai <iwulai@qq.com>
2019-04-23 22:36:31 +0800

    init

--- src/Exceptions/UnauthorizedException.php
+++ src/Exceptions/UnauthorizedException.php
@@ -6,9 +6,7 @@ use Larfree\ApiResponse;
 
 class UnauthorizedException extends ApiException
 {
-    protected $message = '用户认证错误！未登录。';
+    protected $message = '认证异常！未登录。';
 
     protected $status = ApiResponse::HTTP_UNAUTHORIZED;
-
-    protected $code = 40101;
 }
