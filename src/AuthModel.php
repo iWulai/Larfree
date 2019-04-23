ff900f7d0594a2b8e64b3cc72c68e148b21de214
iwulai <iwulai@qq.com>
2019-04-23 14:35:54 +0800

    init

--- src/AuthModel.php
+++ src/AuthModel.php
@@ -3,7 +3,6 @@
 namespace Larfree;
 
 use Illuminate\Auth\Authenticatable;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Contracts\Auth\Authenticatable as Authenticate;
 
 abstract class AuthModel extends Model implements Authenticate
