97a2ed7133940e17970bc7efe087881bdf6b8027
iwulai <iwulai@qq.com>
2019-04-24 19:49:03 +0800

    init

--- src/AuthModel.php
+++ src/AuthModel.php
@@ -3,22 +3,11 @@
 namespace Larfree;
 
 use Illuminate\Auth\Authenticatable;
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Contracts\Auth\Authenticatable as Authenticate;
 
-abstract class AuthModel extends Model implements Authenticate, JWTSubject
+abstract class AuthModel extends Model implements Authenticate
 {
     use Authenticatable;
 
     protected $hidden = ['password'];
-
-    public function getJWTIdentifier()
-    {
-        return $this->getKey();
-    }
-
-    public function getJWTCustomClaims()
-    {
-        return [];
-    }
 }
