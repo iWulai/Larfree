c13d75f68c6a5941f2d6102c80050e9353c2d8ed
iwulai <iwulai@qq.com>
2019-04-24 20:10:59 +0800

    init

--- src/Auth/UserAuth.php
+++ src/Auth/UserAuth.php
@@ -3,8 +3,19 @@
 namespace Larfree\Auth;
 
 use Larfree\AuthModel;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserAuth extends AuthModel
+class UserAuth extends AuthModel implements JWTSubject
 {
     protected $table = 'users';
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
