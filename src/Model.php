0b2e85e2104c1f8a043f8463be2b9ee64905a1db
iwulai <iwulai@qq.com>
2019-04-25 10:54:48 +0800

    init

--- src/Model.php
+++ src/Model.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 abstract class Model extends BaseModel
 {
+    protected $dates = ['created_at', 'updated_at', 'deleted_at'];
+
     public function newInstance($attributes = [], $exists = false)
     {
         return parent::newInstance($attributes, $exists)->setAppends($this->appends);
